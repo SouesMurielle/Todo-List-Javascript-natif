4dae95d0b016cec3dfd1ddb8424b6cc5e79a7e33
SouesMurielle <52404196+SouesMurielle@users.noreply.github.com>
2020-12-19 14:11:26 +0100

    Update

---
+++ main.js
@@ -0,0 +1,30 @@
+let btn = document.getElementById('button');
+let input = document.querySelector('#new-list-name');
+let listDiv = document.querySelector('#lists-list');
+let arrayList = [];
+
+class List {
+    constructor(listName, items= []) {
+        this.listName = listName;
+        this.items = items;
+    }
+}
+
+btn.addEventListener('click', addList);
+
+function addList() {
+    if (input.value === '') {
+        input.placeholder= "Please choose a name";
+    } else {
+        arrayList.push(new List(input.value, []));
+        let newButton = document.createElement('button');
+        newButton.innerText = arrayList[arrayList.length-1].listName;
+        listDiv.append(newButton);
+    }
+
+}
+
+//let paragraph = document.querySelector('#info');
+// paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season
+//
+// Total viewing time: ${totalShowTime} minutes`
